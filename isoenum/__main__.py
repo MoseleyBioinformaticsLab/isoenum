4bb1359d987d5877910088cb9fde5abee31a858e
Andrey Smelter <andrey.smelter@gmail.com>
2018-04-06 11:59:21 -0400

    Added __main__.py file.

---
+++ isoenum/__main__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import docopt
+
+from . import cli
+from . import __version__
+
+if __name__ == '__main__':
+    args = docopt.docopt(cli.__doc__, version=__version__)
+    cli.cli(args)
