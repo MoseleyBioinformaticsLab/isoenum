a68510189290246847f210d8fb074b25dcc1a809
Andrey Smelter <andrey.smelter@gmail.com>
2018-04-06 12:25:24 -0400

    Added configuration files processing module.

---
+++ isoenum/conf.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+#  -*- coding: utf-8 -*-
+
+"""
+isoenum.conf
+~~~~~~~~~~~~
+
+This module provides configuration files necessary for 
+isotopic enumerator.
+"""
+
+import os
+import json
+
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+isotopes_path = '{}/config_files/isotopes.json'.format(this_directory)
+
+with open(isotopes_path, 'r') as infile:
+    isotopes_conf = json.load(infile)
