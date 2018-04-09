13478a648961e84d2e9c8a0331dda7193e024fc2
Andrey Smelter <andrey.smelter@gmail.com>
2018-04-09 01:20:04 -0400

    Added package documentation string.

--- isoenum/__init__.py
+++ isoenum/__init__.py
@@ -1,4 +1,30 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+
+"""This package provides routines to generate isotopically-resolved ``InChI`` 
+(International Chemical Identifier) from non isotopically labeled ``CTfile``
+formatted files (e.g. ``Molfiles``, ``SDfiles``) or non isotopically labeled 
+InChI identifiers.
+
+This package includes the following modules:
+
+``cli``
+    This module provides command-line interface for the ``isoenum`` package.
+
+``iso_property``
+    This module provides the :func:`~isoenum.iso_property.create_iso_property` 
+    function that is responsible for generating ``Ctab`` properties block (part of 
+    ``CTfile`` formatted files specifying "ISO" property for atoms) based on 
+    provided labeling schema.
+
+``labeling_schema``
+    This module provides functions foe generating labeling schema.
+
+``openbabel``
+    This module provides functions to call Open Babel software to convert 
+    between ``InChI`` and ``CTfile`` formatted files. 
+"""
+
+
 __version__ = '0.1.0'
