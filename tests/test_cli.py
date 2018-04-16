a3d03567ecdc09d73ac7e38b7809d34db5167eb4
Andrey Smelter <andrey.smelter@gmail.com>
2018-04-16 17:02:07 -0400

    Added basic cli tests.

---
+++ tests/test_cli.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import pytest
+
+
+@pytest.mark.parametrize('path, parameters', [
+    ('-a C-13', 'tests/example_data/bmse000040.mol'),
+    ('-e C-13', 'tests/example_data/bmse000040.mol'),
+    ('-a C-13', 'tests/example_data/bmse000040.sdf'),
+    ('-e C-13', 'tests/example_data/bmse000040.sdf'),
+    ('-a C-13', 'tests/example_data/bmse000040.inchi'),
+    ('-e C-13', 'tests/example_data/bmse000040.inchi')
+])
+def test_name_command(path, parameters):
+    command = "python3 -m isoenum name {} {}".format(path, parameters)
+    assert os.system(command) == 0
