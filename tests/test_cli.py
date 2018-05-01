85dd69355564f05d9e43a710d6e5cbcc865e27ea
Andrey Smelter <andrey.smelter@gmail.com>
2018-05-01 18:08:49 -0400

    Added -s cli option

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -8,6 +8,7 @@ import pytest
 @pytest.mark.parametrize('path, parameters', [
     ('-a C-13', 'tests/example_data/bmse000040.mol'),
     ('-e C-13', 'tests/example_data/bmse000040.mol'),
+    ('-s C-13-2', 'tests/example_data/bmse000040.mol'),
     ('-a C-13', 'tests/example_data/bmse000040.sdf'),
     ('-e C-13', 'tests/example_data/bmse000040.sdf'),
     ('-a C-13', 'tests/example_data/bmse000040.inchi'),
