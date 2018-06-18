318929eec25f7b27c6ad1b0afe3b6a94618ad7ca
Andrey Smelter <andrey.smelter@gmail.com>
2018-06-18 15:38:16 -0400

    PEP8 line breaks.

--- isoenum/__main__.py
+++ isoenum/__main__.py
@@ -6,9 +6,11 @@ import docopt
 from . import cli
 from . import __version__
 
+
 def main():
     args = docopt.docopt(cli.__doc__, version=__version__)
     cli.cli(args)
+
 
 if __name__ == '__main__':
     main()
