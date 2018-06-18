136a3780e055aac27cd62a204a19f575b8663e1c
Andrey Smelter <andrey.smelter@gmail.com>
2018-06-18 13:24:29 -0400

    Added entry-point main() function.

--- isoenum/__main__.py
+++ isoenum/__main__.py
@@ -6,6 +6,9 @@ import docopt
 from . import cli
 from . import __version__
 
-if __name__ == '__main__':
+def main():
     args = docopt.docopt(cli.__doc__, version=__version__)
     cli.cli(args)
+
+if __name__ == '__main__':
+    main()
