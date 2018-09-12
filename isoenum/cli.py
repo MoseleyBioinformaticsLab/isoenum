20ac05d07de1f268e92bfbeed3ae192339df0336
Andrey Smelter <andrey.smelter@gmail.com>
2018-09-12 14:33:55 -0400

    Added extension against default output formats.

--- isoenum/cli.py
+++ isoenum/cli.py
@@ -357,7 +357,7 @@ def save_output(outputstr, path, file_format):
         dirpath, basename = os.path.split(os.path.normpath(path))
         filename, extension = os.path.splitext(basename)
 
-        if not extension:
+        if not extension or extension not in output_formats_conf:
             extension = '.{}'.format(file_format)
 
         filename = '{}{}'.format(filename, extension)
