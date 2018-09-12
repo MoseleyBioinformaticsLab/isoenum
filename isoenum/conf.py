9b137c492ce23329c0994ab4a6e9b13e8a45deaf
Andrey Smelter <andrey.smelter@gmail.com>
2018-09-12 14:21:35 -0400

    Load formats configuration file.

--- isoenum/conf.py
+++ isoenum/conf.py
@@ -15,6 +15,10 @@ import json
 
 this_directory = os.path.abspath(os.path.dirname(__file__))
 isotopes_path = '{}/config_files/isotopes.json'.format(this_directory)
+formats_path = '{}/config_files/formats.json'.format(this_directory)
 
 with open(isotopes_path, 'r') as infile:
     isotopes_conf = json.load(infile)
+
+with open(formats_path, 'r') as infile:
+    formats_conf = set(json.load(infile))
