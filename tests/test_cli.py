74554463529233b33cb277647fccbdba9f04b373
Andrey Smelter <andrey.smelter@gmail.com>
2018-10-19 11:20:47 -0400

    Remove inchi from testing.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -23,21 +23,21 @@ def teardown_module(module):
     ('tests/example_data/bmse000040.sdf', '-a 13:C'),
     ('tests/example_data/bmse000040.sdf', '-e 13:C'),
     ('tests/example_data/bmse000040.sdf', '-s 13:C:2'),
-    ('tests/example_data/bmse000040.inchi', '-a 13:C'),
-    ('tests/example_data/bmse000040.inchi', '-e 13:C'),
-    ('tests/example_data/bmse000040.inchi', '-s 13:C:2'),
+    #('tests/example_data/bmse000040.inchi', '-a 13:C'),
+    #('tests/example_data/bmse000040.inchi', '-e 13:C'),
+    #('tests/example_data/bmse000040.inchi', '-s 13:C:2'),
     ('tests/example_data/bmse000040.mol', '-a 13:C -s 13:C:2 -e 15:N'),
     ('tests/example_data/bmse000040.sdf', '-a 13:C -s 13:C:2 -e 15:N'),
-    ('tests/example_data/bmse000040.inchi', '-a 13:C -s 13:C:2 -e 15:N'),
+    (#'tests/example_data/bmse000040.inchi', '-a 13:C -s 13:C:2 -e 15:N'),
     ('https://raw.githubusercontent.com/MoseleyBioinformaticsLab/isoenum/master/tests/example_data/bmse000040.mol', '-a 13:C'),
     ('https://raw.githubusercontent.com/MoseleyBioinformaticsLab/isoenum/master/tests/example_data/bmse000040.mol', '-e 13:C'),
     ('https://raw.githubusercontent.com/MoseleyBioinformaticsLab/isoenum/master/tests/example_data/bmse000040.mol', '-s 13:C:2'),
     ('https://raw.githubusercontent.com/MoseleyBioinformaticsLab/isoenum/master/tests/example_data/bmse000040.sdf', '-a 13:C'),
     ('https://raw.githubusercontent.com/MoseleyBioinformaticsLab/isoenum/master/tests/example_data/bmse000040.sdf', '-e 13:C'),
-    ('https://raw.githubusercontent.com/MoseleyBioinformaticsLab/isoenum/master/tests/example_data/bmse000040.sdf', '-s 13:C:2'),
-    ('https://raw.githubusercontent.com/MoseleyBioinformaticsLab/isoenum/master/tests/example_data/bmse000040.inchi', '-a 13:C'),
-    ('https://raw.githubusercontent.com/MoseleyBioinformaticsLab/isoenum/master/tests/example_data/bmse000040.inchi', '-e 13:C'),
-    ('https://raw.githubusercontent.com/MoseleyBioinformaticsLab/isoenum/master/tests/example_data/bmse000040.inchi', '-s 13:C:2')
+    ('https://raw.githubusercontent.com/MoseleyBioinformaticsLab/isoenum/master/tests/example_data/bmse000040.sdf', '-s 13:C:2')
+    #('https://raw.githubusercontent.com/MoseleyBioinformaticsLab/isoenum/master/tests/example_data/bmse000040.inchi', '-a 13:C'),
+    #('https://raw.githubusercontent.com/MoseleyBioinformaticsLab/isoenum/master/tests/example_data/bmse000040.inchi', '-e 13:C'),
+    #('https://raw.githubusercontent.com/MoseleyBioinformaticsLab/isoenum/master/tests/example_data/bmse000040.inchi', '-s 13:C:2')
 ])
 def test_name_command(path, parameters):
     command = "python -m isoenum name {} {}".format(path, parameters)
