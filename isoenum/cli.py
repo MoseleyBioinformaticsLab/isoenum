45164438142ecdde08ffb8cb15d0b3fd3c14f78f
andreysmelter <andrey.smelter@gmail.com>
2019-08-14 17:03:51 -0400

    Update cli module.

--- isoenum/cli.py
+++ isoenum/cli.py
@@ -83,38 +83,53 @@ def cli(cmdargs):
     :return: None.
     :rtype: :py:obj:`None`
     """
-    path_or_id = cmdargs['<path-to-ctfile-file-or-inchi-file-or-inchi-string>']
+    path_or_id = cmdargs["<path-to-ctfile-file-or-inchi-file-or-inchi-string>"]
 
-    if cmdargs['name']:
-        sdfile = api.iso(path_or_id=path_or_id,
-                         specific_opt=cmdargs['--specific'],
-                         all_opt=cmdargs['--all'],
-                         enumerate_opt=cmdargs['--enumerate'],
-                         complete_opt=cmdargs['--complete'],
-                         ignore_iso_opt=cmdargs['--ignore-iso'])
+    if cmdargs["name"]:
+        sdfile = api.iso(
+            path_or_id=path_or_id,
+            specific_opt=cmdargs["--specific"],
+            all_opt=cmdargs["--all"],
+            enumerate_opt=cmdargs["--enumerate"],
+            complete_opt=cmdargs["--complete"],
+            ignore_iso_opt=cmdargs["--ignore-iso"],
+        )
 
-        create_output(sdfile=sdfile, path=cmdargs['--output'], file_format=cmdargs['--format'])
+        create_output(
+            sdfile=sdfile, path=cmdargs["--output"], file_format=cmdargs["--format"]
+        )
 
-    elif cmdargs['ionize']:
-        atom_states = cmdargs['--state']
+    elif cmdargs["ionize"]:
+        atom_states = cmdargs["--state"]
         sdfile = api.chg(path_or_id=path_or_id, atom_states=atom_states)
-        create_output(sdfile=sdfile, path=cmdargs['--output'], file_format=cmdargs['--format'])
+        create_output(
+            sdfile=sdfile, path=cmdargs["--output"], file_format=cmdargs["--format"]
+        )
 
-    elif cmdargs['nmr']:
-        experiment_type = cmdargs['--type']
-        decoupled = [element.upper() for element in cmdargs['--decoupled']]
-        couplings = [coupling.upper() for coupling in cmdargs['--jcoupling']]
-        subset = cmdargs['--subset']
+    elif cmdargs["nmr"]:
+        experiment_type = cmdargs["--type"]
+        decoupled = [element.upper() for element in cmdargs["--decoupled"]]
+        couplings = [coupling.upper() for coupling in cmdargs["--jcoupling"]]
+        subset = cmdargs["--subset"]
 
-        sdfile = api.iso_nmr(path_or_id=path_or_id, experiment_type=experiment_type,
-                             couplings=couplings, decoupled=decoupled, subset=subset)
+        sdfile = api.iso_nmr(
+            path_or_id=path_or_id,
+            experiment_type=experiment_type,
+            couplings=couplings,
+            decoupled=decoupled,
+            subset=subset,
+        )
 
-        create_output(sdfile=sdfile, path=cmdargs['--output'], file_format=cmdargs['--format'])
+        create_output(
+            sdfile=sdfile, path=cmdargs["--output"], file_format=cmdargs["--format"]
+        )
 
-    elif cmdargs['vis']:
-        api.visualize(path_or_id=path_or_id,
-                      output_path=cmdargs['--output'],
-                      output_format=cmdargs['--format'])
+    elif cmdargs["vis"]:
+        api.visualize(
+            path_or_id=path_or_id,
+            output_path=cmdargs["--output"],
+            output_format=cmdargs["--format"],
+        )
 
 
 def _unpack(param):
@@ -126,7 +141,7 @@ def _unpack(param):
     """
     options = []
     for option_str in param:
-        options.extend(option_str.split(','))
+        options.extend(option_str.split(","))
     return options
 
 
@@ -144,21 +159,21 @@ def save_output(outputstr, path, file_format):
         filename, extension = os.path.splitext(basename)
 
         if not extension or extension.lower() not in output_formats_conf:
-            extension = '.{}'.format(file_format)
+            extension = ".{}".format(file_format)
 
-        filename = '{}{}'.format(filename, extension)
+        filename = "{}{}".format(filename, extension)
         filepath = os.path.join(dirpath, filename)
 
         if dirpath and not os.path.exists(dirpath):
             raise IOError('Directory does not exist: "{}"'.format(dirpath))
 
-        with open(filepath, 'w') as outfile:
+        with open(filepath, "w") as outfile:
             print(outputstr, file=outfile)
     else:
         print(outputstr, file=sys.stdout)
 
 
-def create_output(sdfile, path=None, file_format='inchi'):
+def create_output(sdfile, path=None, file_format="inchi"):
     """Create output containing conversion results.
 
     :param sdfile: ``SDfile`` instance.
@@ -171,39 +186,45 @@ def create_output(sdfile, path=None, file_format='inchi'):
     file_format = file_format.lower()
 
     if file_format not in output_formats_conf:
-        raise ValueError('Unknown output format: "{}".\n'
-                         'Available formats are: {}'.format(file_format, output_formats_conf))
+        raise ValueError(
+            'Unknown output format: "{}".\n'
+            "Available formats are: {}".format(file_format, output_formats_conf)
+        )
 
-    if file_format in {'sdf', 'mol'}:
-        save_output(outputstr=sdfile.writestr(file_format='ctfile'),
-                    path=path,
-                    file_format=file_format)
+    if file_format in {"sdf", "mol"}:
+        save_output(
+            outputstr=sdfile.writestr(file_format="ctfile"),
+            path=path,
+            file_format=file_format,
+        )
 
-    elif file_format in {'json'}:
-        save_output(outputstr=sdfile.writestr(file_format='json'),
-                    path=path,
-                    file_format=file_format)
+    elif file_format in {"json"}:
+        save_output(
+            outputstr=sdfile.writestr(file_format="json"),
+            path=path,
+            file_format=file_format,
+        )
 
-    elif file_format in {'inchi'}:
+    elif file_format in {"inchi"}:
         output = []
         for entry_id in sdfile:
-            output.extend([inchi.strip() for inchi in sdfile[entry_id]['data']['InChI']])
+            output.extend(
+                [inchi.strip() for inchi in sdfile[entry_id]["data"]["InChI"]]
+            )
 
-        save_output(outputstr='\n'.join(output),
-                    path=path,
-                    file_format=file_format)
+        save_output(outputstr="\n".join(output), path=path, file_format=file_format)
 
-    elif file_format in {'csv'}:
+    elif file_format in {"csv"}:
         outputstr = io.StringIO()
-        csvwriter = csv.writer(outputstr, delimiter='\t')
+        csvwriter = csv.writer(outputstr, delimiter="\t")
 
         for entry_id in sdfile:
             csv_data = []
-            for data_id in sdfile[entry_id]['data']:
-                value = ' + '.join([item.strip() for item in sdfile[entry_id]['data'][data_id]])
+            for data_id in sdfile[entry_id]["data"]:
+                value = " + ".join(
+                    [item.strip() for item in sdfile[entry_id]["data"][data_id]]
+                )
                 csv_data.append(value)
             csvwriter.writerow(csv_data)
 
-        save_output(outputstr=outputstr.getvalue(),
-                    path=path,
-                    file_format=file_format)
+        save_output(outputstr=outputstr.getvalue(), path=path, file_format=file_format)
