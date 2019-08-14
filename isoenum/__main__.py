6b9eadcc5282a64e89ff7fceba108f5380414d8b
andreysmelter <andrey.smelter@gmail.com>
2019-08-14 15:39:28 -0400

    Added function to preprocess docopt cli.

--- isoenum/__main__.py
+++ isoenum/__main__.py
@@ -7,9 +7,36 @@ from . import cli
 from . import __version__
 
 
+def _remove_new_line_from_usage_patterns(docstr):
+    """Removes new line from usage patterns in docopt CLI description.
+    :param str docstr: docopt CLI description.
+    :return: Reformatted docstring ready for docopt consumption.
+    :rtype: :py:class:`str`
+    """
+    lines = []
+
+    usage = False
+    for line in docstr.split("\n"):
+        if line.startswith("Usage:"):
+            usage = True
+        elif line.startswith("Options:"):
+            lines.append("")
+            usage = False
+
+        if usage:
+            if not line:
+                continue
+            else:
+                lines.append(line)
+        else:
+            lines.append(line)
+
+    return "\n".join(lines)
+
+
 def main():
-    args = docopt.docopt(cli.__doc__, version=__version__)
-    cli.cli(args)
+    docstr = _remove_new_line_from_usage_patterns(docstr=cli.__doc__)
+    cli.cli(docopt.docopt(doc=docstr, version=__version__))
 
 
 if __name__ == '__main__':
